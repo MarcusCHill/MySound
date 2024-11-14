45c031a7cd373e9d393df5d5517b2c2127f6b69e
Marcus Hill <hillmarcus32@gmail.com>
2024-11-14 18:19:17 -0500

    display user and songs data

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import Spotify from './services/spotify';
 const App = () => {
   return (
     <div className="App">
-      <h1>Spotify OAuth Authentication</h1>
       <Spotify/>
     </div>
   );
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,53 +1,99 @@
 import React, { useEffect, useState } from 'react';
-import axios from 'axios';
+import { fetchSpotifyData } from '../services/fetchSpotifyData';
 
 const Home = ({ accessToken }) => {
+  const [loading, setLoading] = useState(true)
+  const [error, setError] = useState(null)
   const [userData, setUserData] = useState(null);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
+  const [topAllTimeTracks, setTopAllTimeTrakts] = useState(null);
+  const [topSixMonthsTracks, setTopSixMonthsTracks] = useState(null);
+  const [topMonthTracks, setTopMonthTracks] = useState(null);
 
-  // Fetch user data from Spotify API
+  // Fetch user data and top tracks from Spotify API
   useEffect(() => {
-    if (accessToken) {
-      axios
-        .get('https://api.spotify.com/v1/me', {
-          headers: {
-            Authorization: `Bearer ${accessToken}`,
-          },
-        })
-        .then((response) => {
-          setUserData(response.data);
-          setLoading(false);
-        })
-        .catch((err) => {
-          setError(err);
-          setLoading(false);
-        });
-    }
-  }, [accessToken]);
 
+    const fetchData = async () => {
+      if (accessToken) {
+        try{
+          const userDataResponse = await fetchSpotifyData(accessToken, 'me')
+          setUserData(userDataResponse)
+
+          const TATResponse = await fetchSpotifyData(accessToken, 'me/top/tracks?limit=50&time_range=long_term')
+          setTopAllTimeTrakts(TATResponse)
+
+          const TSMResponse = await fetchSpotifyData(accessToken, 'me/top/tracks?limit=50&time_range=medium_term')
+          setTopSixMonthsTracks(TSMResponse)
+
+          const TMResponse = await fetchSpotifyData(accessToken, 'me/top/tracks?limit=50&time_range=short_term')
+          setTopMonthTracks(TMResponse)
+
+          setLoading(false)
+        } catch (error){
+          setLoading(false)
+          setError(error)
+        }
+      }
+    }
+
+    fetchData();
+
+  }, [accessToken]);
+  
   if (loading) {
     return <div>Loading...</div>;
   }
 
   if (error) {
-    return <div>Error fetching user data: {error.message}</div>;
-  }
-
-  if (!userData) {
-    return <div>No user data found</div>;
+    return <h1>Error fetching spotify data.</h1>;
   }
 
   return (
     <div>
-      <h2>User Data from Spotify</h2>
-      <ul>
-        <li><strong>Name:</strong> {userData.display_name}</li>
-        <li><strong>Email:</strong> {userData.email}</li>
-        <li><strong>Followers:</strong> {userData.followers.total}</li>
-        <li><strong>Country:</strong> {userData.country}</li>
-        <li><strong>Profile Image:</strong> <img src={userData.images[0]?.url} alt="Profile" width="50" height="50" /></li>
-      </ul>
+      <h1>My Sound</h1>
+      <div>
+        <div>
+          <img src={userData.images[0]?.url} alt="Profile" width="50" height="50" />
+          <div>
+            <h3>GOAT Song:</h3>
+            <h4>{topAllTimeTracks.items[0].name} - {topAllTimeTracks.items[0].album.name}</h4>
+            <h3>6 Month Leader:</h3>
+            <h4>{topSixMonthsTracks.items[0].name} - {topSixMonthsTracks.items[0].album.name}</h4>
+            <h3>Hot Right Now:</h3>
+            <h4>{topMonthTracks.items[0].name} - {topMonthTracks.items[0].album.name}</h4>
+          </div>
+        </div>
+        <h2>{userData.display_name}</h2>
+      </div>
+      <section>
+        <h2>Top 50: All Time</h2>
+        <ul>
+          {topAllTimeTracks.items.map((song, index) => (
+            <li key={index}>
+              {song.name} - {song.album.name}
+            </li>
+          ))}
+        </ul>
+      </section>
+      <section>
+        <h2>Top 50: Last Six Month</h2>
+        <ul>
+          {topSixMonthsTracks.items.map((song, index) => (
+            <li key={index}>
+              {song.name} - {song.album.name}
+            </li>
+          ))}
+        </ul>
+      </section>
+      <section>
+        <h2>Top 50: Last Month</h2>
+        <ul>
+          {topMonthTracks.items.map((song, index) => (
+            <li key={index}>
+              {song.name} - {song.album.name}
+            </li>
+          ))}
+        </ul>
+      </section>
     </div>
   );
 };
