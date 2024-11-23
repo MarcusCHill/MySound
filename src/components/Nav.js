7fae18ed8d3ede2c61294aea603014c715e38c62
Marcus Hill <hillmarcus32@gmail.com>
2024-11-23 00:09:42 -0500

    Ensure handleSignOut is called onClick

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -16,7 +16,7 @@ export function Nav({ isActive, handleClick }) {
             {(currentPath !== '/callback/all-time') && <Link to="/callback/all-time" onClick={handleClick}>All Time</Link>}
             {(currentPath !== '/callback/six-months') && <Link to="/callback/six-months" onClick={handleClick}>Last Six Months</Link>}
             {(currentPath !== '/callback/last-month') && <Link to="/callback/last-month" onClick={handleClick}>Last Month</Link>}
-            <Link to="/" onClick={[handleClick, handleSignOut]}>Sign Out</Link>
+            <Link to="/" onClick={() => { handleClick(); handleSignOut(); }}>Sign Out</Link>
         </div>
         </div>
     )
